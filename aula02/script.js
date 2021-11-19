18caee21cb6df5de454a6f06ef50fff1dadb89ed
Luis Felipe <luisfelipeosouzati@gmail.com>
2021-11-19 20:53:22 -0300

    Arquivos adicionados
    Comitado

---
+++ aula02/script.js
@@ -0,0 +1,85 @@
+// Tipos primitivos
+
+// Boolean
+/*let v_f = false;
+console.log(typeof(v_f));
+
+// Number
+let numero = 5;
+console.log(typeof(numero));
+
+// String
+let nome = 'Raimundão';
+console.log(typeof(nome))
+
+// Função
+let funcao = function(){}
+console.log(typeof(funcao))*/
+
+// Como declarar
+/*let variavel = 'Carlão'
+variavel = 'Carlinhos'
+console.log(variavel)*/
+
+/*const constante = 50
+console.log(constante)*/
+
+// Escopo
+/*var escopoGlobal = 'global'
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local'
+    console.log(escopoLocalInterno)
+}
+
+escopoLocal()*/
+
+// Atribuição
+/*var atribuição = 'recebida'
+console.log(atribuição);
+
+// Comparação entre valores 
+// == -> Testa só o valor
+var n1 = '0'
+var n2 = 0
+console.log(n1 == n2)*/
+
+// === -> Testa o valor e o tipo!
+/*var n1 = '0'
+var n2 = 0
+console.log(n1 === n2)*/
+
+// Operadores aritméticos
+// Adição
+/*var n1 = 2;
+var n2 = 5;
+console.log(n1 + n2) */
+
+// Subtração
+/*var n1 = 5
+var n2 = 4
+console.log(n1 - n2)*/
+
+// Multiplicação
+/*var n1 = 5
+var n2 = 5
+console.log(n1 * n2)*/
+
+// Divisão
+var n1 = 2
+var n2 = 2
+console.log(n1 / n2)
+
+// Operadores relacionais
+var maiorQue = 2 > 1
+console.log(maiorQue)
+
+var menorQue = 5 < 10
+console.log(menorQue)
+
+var maiorOuIgual = 5 >= 5
+console.log(maiorOuIgual)
+
+var menorOuIgual = 5 <= 5
+console.log(menorOuIgual)
