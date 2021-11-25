240c42072387c5879edf38c27e3d1483444fcb13
Luis Felipe <luisfelipeosouzati@gmail.com>
2021-11-25 09:29:16 -0300

    Adicionado o assunto de condições em js

---
+++ aula04/script.js
@@ -0,0 +1,11 @@
+var jogador1 = 0
+var jogador2 = 0
+var placar;
+
+if(jogador1 > 0){
+    console.log(`O jogador 1 marcou ${jogador1} pontos`)
+} else if(jogador2 > 0){
+    console.log(`O jogador 2 marcou ${jogador2} pontos`)
+} else {
+    console.log('Ninguém marcou pontos')
+}
